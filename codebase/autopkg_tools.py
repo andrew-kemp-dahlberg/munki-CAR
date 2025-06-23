e0c53c06a1a2061550386ee5b10c2a9107bd07c2
Andrew Kemp-Dahlberg <akempdah@gmail.com>
2025-06-22 22:04:58 -0500

    test

--- codebase/autopkg_tools.py
+++ codebase/autopkg_tools.py
@@ -624,13 +624,13 @@ class Recipe:
                 REPORT_PLIST_PATH,
             ]
             
-            ### Added by Andrew to properly check if items are new downloads or sparse files ###
-            if not os.path.exists(METADATA_CACHE_PATH) :
-                log.info("METADATA_CACHE_PATH does not exist, using VirusTotalAnalyzer post processor")
-                ### End of Andrew's modifications to autopkg_tools.py ###          
-                ### Added from Gusto's autopkg_tools.py except they do it in cmd  ###
-                cmd.extend(["--post", "io.github.hjuutilainen.VirusTotalAnalyzer/VirusTotalAnalyzer"])
-                #### End of Gusto's autopkg_tools.py modifications although virus analyzer functionality should be investigated potentially as CI####
+            # ### Added by Andrew to properly check if items are new downloads or sparse files ###
+            # if not os.path.exists(METADATA_CACHE_PATH) :
+            #     log.info("METADATA_CACHE_PATH does not exist, using VirusTotalAnalyzer post processor")
+            #     ### End of Andrew's modifications to autopkg_tools.py ###          
+            #     ### Added from Gusto's autopkg_tools.py except they do it in cmd  ###
+            #     cmd.extend(["--post", "io.github.hjuutilainen.VirusTotalAnalyzer/VirusTotalAnalyzer"])
+            #     #### End of Gusto's autopkg_tools.py modifications although virus analyzer functionality should be investigated potentially as CI####
 
 
 
